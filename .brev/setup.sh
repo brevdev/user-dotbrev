abb27615c3703e3a874375ce101dc7a8987f6c27
ali-wetrill <ali.ahmed@berkeley.edu>
2022-03-20 22:12:06 +0000

    updated user settings including extensions

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -26,12 +26,12 @@ code-server --install-extension vsls-contrib.codetour
 code-server --install-extension ms-azuretools.vscode-docker
 code-server --install-extension mikestead.dotenv
 code-server --install-extension dbaeumer.vscode-eslint
-code-server --install-extension golang.go
+# code-server --install-extension golang.go
 code-server --install-extension ephoton.indent-switcher
 code-server --install-extension k--kato.intellij-idea-keybindings
 code-server --install-extension davidanson.vscode-markdownlint
-code-server --install-extension pkief.material-icon-theme
-code-server --install-extension foxundermoon.shell-format
+# code-server --install-extension pkief.material-icon-theme
+# code-server --install-extension foxundermoon.shell-format
 code-server --install-extension tyriar.sort-lines
 code-server --install-extension xr0master.webstorm-intellij-darcula-theme
 
