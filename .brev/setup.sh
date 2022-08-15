8df63ab3569df17dd0b85ca6d88f18a36b670f5c
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-08-15 16:14:30 -0700

    shrug

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -4,5 +4,7 @@ set -eo pipefail
 
 ## install and configure oh-my-zsh headless for ubuntu 20.04
 sudo apt update && sudo apt install -y zsh
-sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+
+# configure zsh as default shell without getting prompted for password
+sudo usermod -s /bin/zsh $USER
