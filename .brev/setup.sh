20573e03dc7e664a0fc407e2ad8bee70040e2524
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-08-15 16:28:57 -0700

    configure setting.json on remote machines to use zsh and default shell to be zsh

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -8,3 +8,39 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # configure zsh as default shell without getting prompted for password
 sudo usermod -s /bin/zsh $USER
+
+# add `"terminal.integrated.defaultProfile.linux": "zsh"` to .vscode-server/data/Machine/settings.json
+python3 - <<EOF
+import json
+import os
+def create_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def create_file_if_not_exists(path):
+    if not os.path.exists(path):
+        open(path, 'a').close()
+
+create_if_not_exists('.vscode-server')
+create_if_not_exists('.vscode-server/data')
+create_if_not_exists('.vscode-server/data/Machine')
+create_file_if_not_exists('.vscode-server/data/Machine/settings.json')
+
+with open('.vscode-server/data/Machine/settings.json', 'r') as f:
+    try:
+        data = json.load(f)
+    except:
+        data = {}
+    data['terminal.integrated.defaultProfile.linux'] = 'zsh'
+with open('.vscode-server/data/Machine/settings.json', 'w') as f:
+    json.dump(data, f, indent=2)
+EOF
+
+## install and configure vim
+sudo apt update && sudo apt install -y vim
+
+## set default editor to vim
+sudo update-alternatives --set editor /usr/bin/vim.basic
+
+## set git editor to vim
+git config --global core.editor vim
