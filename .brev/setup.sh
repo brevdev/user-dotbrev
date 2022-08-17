6eacf3f5e98fc76614c078bd62a52cfe7b4d3af0
r33drichards <57335981+r33drichards@users.noreply.github.com>
2022-08-17 15:29:32 -0700

    remove extra

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -6,13 +6,8 @@ set -eo pipefail
 sudo apt update && sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
-## upate pam to not need password to change shell
-sudo sed -i 's/^auth sufficient pam_shells.so/#auth sufficient pam_shells.so/' /etc/pam.d/chsh
-
 # set default shell to zsh
 sudo chsh -s /bin/zsh $USER
-
-# sudo sed -i 's/\/bin\/bash/\/bin\/zsh/g' /etc/passwd
 
 # add `"terminal.integrated.defaultProfile.linux": "zsh"` to .vscode-server/data/Machine/settings.json
 python3 - <<EOF
@@ -48,6 +43,3 @@ sudo apt update && sudo apt install -y vim
 sudo update-alternatives --set editor /usr/bin/vim.basic
 
 ## set git editor to vim
-git config --global core.editor vim
-
-# untar brev.tar.gz to brev
